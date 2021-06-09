ebb42e4c64c13571b7b2231760f87f0a364aa352
sarthaksd07 <sarthaksd07@gmail.com>
2021-06-09 18:15:46 +0530

    Added:Compute a+b*c

--- sorting.sh
+++ sorting.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -x
 
-read -p "Enter a:" num1
-read -p "Enter b:" num2
-read -p "Enter c:" num3
+read -p "Enter a :" num1
+read -p "Enter b :" num2
+read -p "Enter c :" num3
+
+result=$(( $num1 + $num2 * $num3));
+echo "$result"
