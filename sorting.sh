ae6e9eded84f7302f6cf296986284388b5cbb0ee
sarthaksd07 <sarthaksd07@gmail.com>
2021-06-09 21:55:11 +0530

    Added:Compute c+a/b

--- sorting.sh
+++ sorting.sh
@@ -5,7 +5,10 @@ read -p "Enter b :" num2
 read -p "Enter c :" num3
 
 result=$(( $num1 + $num2 * $num3));
-echo "expression 1:$result"
+echo "expression 1 : $result"
 
 result=$(($num1*$num2+$num3));
-echo "expression 2:$result"
+echo "expression 2 : $result"
+
+result=$(($num3+$num1/$num2));
+echo "expression 3 : $result"
